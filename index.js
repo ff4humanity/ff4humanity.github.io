f89c3703d5da93cc08a00962e6bc719b3ce57dde
ff4humanity <89574424+ff4humanity@users.noreply.github.com>
2021-12-14 02:16:39 +0000

    fix broken service worker if served from subdir

---
+++ index.js
@@ -0,0 +1,23 @@
+(function () {
+  let rootpath = location.pathname.slice(1);
+  if (rootpath.indexOf("/") != -1) {
+    rootpath = "/" + rootpath.split("/")[0] + "/";
+  } else {
+    rootpath = "/";
+  }
+  document
+    .querySelector(".header")
+    .addEventListener("dblclick", toggleFullScreen);
+  navigator.serviceWorker.register(`${rootpath}service-worker.js`, {
+    scope: rootpath,
+  });
+  function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  }
+})();
